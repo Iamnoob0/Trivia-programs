cade542b92ca02a69f45d217e34ebbc05363e1f8
Iamnoob0 <63848285+Iamnoob0@users.noreply.github.com>
2021-10-14 10:54:16 +0700

    Hangman game version 2 2
    Adding post-gameplay actions like Dancing man

---
+++ Hangman_2_2.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+
+using namespace std;
+
+void clearScreen() {
+	for (int i = 0; i < 30; i++) cout << endl;
+}
+
+const string FIGURE[] = {
+        "   -------------    \n"
+        "   |                \n"
+        "   |                \n"
+        "   |                \n"
+        "   |                \n"
+        "   |     \n"
+        " -----   \n",
+        "   -------------    \n"
+        "   |           |    \n"
+        "   |                \n"
+        "   |                \n"
+        "   |                \n"
+        "   |     \n"
+        " -----   \n",
+        "   -------------    \n"
+        "   |           |    \n"
+        "   |           O    \n"
+        "   |                \n"
+        "   |                \n"
+        "   |     \n"
+        " -----   \n",
+        "   -------------    \n"
+        "   |           |    \n"
+        "   |           O    \n"
+        "   |           |    \n"
+        "   |                \n"
+        "   |     \n"
+        " -----   \n",
+        "   -------------    \n"
+        "   |           |    \n"
+        "   |           O    \n"
+        "   |          /|    \n"
+        "   |                \n"
+        "   |     \n"
+        " -----   \n",
+        "   -------------    \n"
+        "   |           |    \n"
+        "   |           O    \n"
+        "   |          /|\\  \n"
+        "   |                \n"
+        "   |     \n"
+        " -----   \n",
+        "   -------------    \n"
+        "   |           |    \n"
+        "   |           O    \n"
+        "   |          /|\\  \n"
+        "   |          /     \n"
+        "   |     \n"
+        " -----   \n",
+        "   -------------    \n"
+        "   |           |    \n"
+        "   |           O    \n"
+        "   |          /|\\  \n"
+        "   |          / \\  \n"
+        "   |     \n"
+        " -----   \n"
+    };
+
+void renderGame(const string& guessedWord, const string& badGuesses);
+void displayFinalResult(bool won, const string& word);
+
+const int MAX_BAD_GUESSES = 7;
+const char DATA_FILE[] = "words.txt";
+
+string chooseWord(const char* fileName);
+char readAGuess();
+bool contains(const string& word, char guess);
+void updateGuessedWord(string& guessedWord, const string& word, char guess);
+
+int main()
+{
+	srand(time(0));
+	string word = chooseWord(DATA_FILE);
+	if (word.length() < 1) {
+		cout << "Error reading vocabulary file " << DATA_FILE;
+		return -1;
+	}
+	string guessedWord = string(word.length(), '-');
+	string badGuesses = "";
+
+	do {
+		renderGame(guessedWord, badGuesses);
+		char guess = readAGuess();
+		if (contains(word, guess))
+			updateGuessedWord(guessedWord, word, guess);
+		else {
+			badGuesses += guess;
+		}
+	} while (badGuesses.length() < MAX_BAD_GUESSES && word != guessedWord);
+    displayFinalResult(badGuesses.length() < MAX_BAD_GUESSES, word);
+
+	return 0;
+}
+
+string getLowerCaseString(const string& s)
+{
+    string res = s;
+    transform(s.begin(), s.end(), res.begin(), ::tolower);
+    return res;
+}
+
+string chooseWord(const char* fileName)
+{
+	vector<string> wordList;
+	ifstream file(fileName);
+  	if (file.is_open()) {
+	    string word;
+		while (file >> word) {
+            wordList.push_back(word);
+        }
+    	file.close();
+  	}
+  	if (wordList.size() > 0) {
+  		int randomIndex = rand() % wordList.size();
+    	return getLowerCaseString(wordList[randomIndex]);
+	} else return "";
+}
+
+char readAGuess()
+{
+	char input;
+	cout << "Your guess: ";
+	cin >> input;
+	return tolower(input);
+}
+
+bool contains(const string& word, char c)
+{
+	return (word.find_first_of(c) != string::npos);
+}
+
+void updateGuessedWord(string& guessedWord, const string& word, char guess)
+{
+	for (int i = word.length() - 1; i >= 0; i--) {
+        if (word[i] == guess) {
+            guessedWord[i] = guess;
+        }
+    }
+}
+
+void displayFinalResult(bool won, const string& word) {
+
+        clearScreen();
+        if (won)
+            cout << "Congratulations! You win!" << endl;
+            
+        else
+            cout << "You lost. The correct word is " << word << endl;
+
+}
+
+void renderGame(const string& guessedWord, const string& badGuesses)
+{
+	clearScreen();
+	int badGuessCount = badGuesses.length();
+	cout << FIGURE[badGuessCount] << endl;
+	cout << "Secret word: " << guessedWord << endl;
+	if (badGuessCount > 0) {
+		cout << "You've made " << badGuessCount << " wrong ";
+		cout << (badGuessCount == 1 ? "guess" : "guesses");
+		cout << ": " << badGuesses << endl;
+	}
+}
