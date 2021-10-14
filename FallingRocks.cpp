f859715e99cef01a09db4891aa36be9d7204c2fa
Iamnoob0 <63848285+Iamnoob0@users.noreply.github.com>
2021-10-14 11:02:10 +0700

    Falling rocks
    Falling rocks from hackerrank
    link: https://www.hackerrank.com/contests/101hack31/challenges/falling-rocks

---
+++ FallingRocks.cpp
@@ -0,0 +1,72 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+int W, H;
+    bool flag = false;
+    char ch = ' ';
+    string st = "";
+    
+    cin >> W >> H;
+    
+    bool A[H+2][W+2];
+    char B[W+2];
+    
+    for (int i = 1; i <= 1; i++) {
+            cin >> st;
+            
+            for (int j = 1; j <= W; j++) {
+                
+                ch = st.at(j - 1);
+                B[j] = ch;
+                
+                if (ch == 'Y') {
+                    A[i][j] = true;
+                } else {
+                    A[i][j] = false;
+                }
+        }
+    }
+    
+    for (int i = 2; i <= H; i++) {
+            cin >> st;
+            
+            for (int j = 1; j <= W; j++) {
+                ch = st.at(j - 1);
+                
+                if (ch == 'R') {
+                    A[i][j] = false;
+                    B[j] = 'R';
+                    
+                } else if (ch == 'E') {
+                    if ((B[j] == 'E' || B[j] == 'Y')
+                            && (A[i - 1][j - 1] || A[i - 1][j] || A[i - 1][j + 1])) {
+                        A[i][j] = true;
+
+                    } else {
+                        A[i][j] = false;
+                    }
+                    B[j] = 'E';
+            }
+
+        }
+    }
+    for (int j = 1; j <= W; j++) {
+            if (A[H][j]) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag) {
+            cout << "YES";
+        } else {
+            cout << "NO";
+        }
+    return 0;
+}
