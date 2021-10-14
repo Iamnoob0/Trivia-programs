2134516bae17bea6a77e9f2d17d4451bac7cbbe2
Iamnoob0 <63848285+Iamnoob0@users.noreply.github.com>
2021-10-14 09:51:10 +0700

    Add files via upload
    GuessIt multiple play

---
+++ GuessIt_1_3.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int high, low;
+int scrtNum;
+int X; 
+
+bool isCharACompExpr(char _ch){
+    return _ch == '>' || _ch == '<' || _ch == '=';
+}
+
+char guessHostAnswer(int _X){
+    char _Answer;
+    
+    do{
+        
+    cout << "Is " << X << " your secret number " << endl;
+    cin >> _Answer;
+    }
+    while(!isCharACompExpr(_Answer));
+    
+    return _Answer;
+}
+
+int selectNumber(int _high, int _low){
+
+    return rand() % (_high - _low + 1) + _low;
+    // return (_high + _low)/2;
+}
+
+int main(){
+    srand(time(0));
+    
+    cout << "Enter the high and low bound, respectively: ";
+    cin >> high >> low;
+    
+    do{
+    cout << "Enter your secret number (between low and high bound): " << endl;
+    cin >> scrtNum;
+    }
+    while( !((scrtNum >= low) && (scrtNum <= high)) );
+    
+    char Answer;
+    
+    do{
+        X = selectNumber(high, low);
+        
+        Answer = guessHostAnswer(X);
+        
+        if(Answer == '>') //X > scrtNum
+            high = X-1;
+        else if(Answer == '<') //X > scrtNum
+            low = X+1;    
+        
+    }
+    while(Answer != '=');
+    
+    cout << "Game over!!!" << endl;
+    cout << "The secret number is " << X << endl;
+    
+    return 0;
+}
